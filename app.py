0c68fc47df9715014ebd872e29e04321a032a8b0
nandhanapsuresh <nandhanapsuresh75@gmail.com>
2026-05-03 09:18:44 +0530

    Day 1-3 works

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import boto3
+from flask import Flask, jsonify
+from flask import request
+
+client = boto3.client('sts')
+print(client.get_caller_identity())
+app = Flask(__name__)
+
+
+REGION = os.environ.get("AWS_REGION", "ap-south-2")
+
+dynamodb = boto3.resource("dynamodb", region_name=REGION)
+courses_table = dynamodb.Table("nandhana-course")
+
+
+@app.route("/courses", methods=["POST"])
+def create_course():
+    data = request.get_json()
+
+    # Basic validation
+    if not data or "id" not in data:
+        return jsonify({"error": "Course ID is required"}), 400
+
+    try:
+        courses_table.put_item(Item=data)
+        return jsonify({"message": "Course created successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/")
+def home():
+    return "OK", 200
+
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok", "service": "course-service"}), 200
+
+
+@app.route("/courses/<course_code>", methods=["GET"])
+def get_course(course_code):
+    resp = courses_table.get_item(Key={"id": course_code})
+    item = resp.get("Item")
+    if not item:
+        return jsonify({"error": "Course not found"}), 404
+    return jsonify(item), 200
+
+
+@app.route("/courses", methods=["GET"])
+def list_courses():
+    resp = courses_table.scan(Limit=50)
+    return jsonify(resp.get("Items", [])), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3001, debug=False)
